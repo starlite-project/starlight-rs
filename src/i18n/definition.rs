2616ac7a203aa030150e239c64e3eab8d30815d5
Gryffon Bellish <owenbellish@gmail.com>
2021-06-07 17:12:44 +0000

    updated macro

--- src/i18n/definition.rs
+++ src/i18n/definition.rs
@@ -1,16 +1,16 @@
 #![allow(dead_code)]
 #[rustfmt::skip]
 pub struct Language {
-    pub ping: &'static str,
     pub pong: &'static str,
+    pub ping: &'static str,
 }
 #[rustfmt::skip]
 impl Language {
-    pub fn ping(&self) -> String {
-        self.ping.to_owned()
-    }
     pub fn pong(&self, latency: &str) -> String {
         self.pong.to_owned()
             .replace("{latency}", latency)
+    }
+    pub fn ping(&self) -> String {
+        self.ping.to_owned()
     }
 }
--- src/lib/state/event_handler.rs
+++ src/lib/state/event_handler.rs
@@ -1,9 +1,13 @@
 use super::State;
 use async_trait::async_trait;
-use twilight_model::gateway::payload::{BanAdd, BanRemove, ChannelCreate, ChannelDelete, ChannelUpdate, ChannelPinsUpdate};
+use twilight_model::gateway::payload::{
+    BanAdd, BanRemove, ChannelCreate, ChannelDelete, ChannelPinsUpdate, ChannelUpdate, GuildCreate,
+    GuildDelete, GuildEmojisUpdate, GuildIntegrationsUpdate, GuildUpdate, InviteCreate,
+    InviteDelete,
+};
 
 macro_rules! impl_handler_trait {
-    ($($fn:ident: $($fn_args:ident)+;)*) => {
+    ($($fn:ident: [$($fn_args:ty),*];)*) => {
             #[async_trait]
             #[allow(unused_variables)]
             pub trait EventHandler: Send + Sync {
@@ -17,12 +21,26 @@ macro_rules! impl_handler_trait {
 }
 
 impl_handler_trait! {
-    ban_add: BanAdd;
-    ban_remove: BanRemove;
-    channel_create: ChannelCreate;
-    channel_delete: ChannelDelete;
-    channel_pins_update: ChannelPinsUpdate;
-    channel_update: ChannelUpdate;
+    ban_add: [BanAdd];
+    ban_remove: [BanRemove];
+    channel_create: [ChannelCreate];
+    channel_delete: [ChannelDelete];
+    channel_pins_update: [ChannelPinsUpdate];
+    channel_update: [ChannelUpdate];
+    gateway_heartbeat: [u64];
+    gateway_heartbeat_ack: [];
+    gateway_hello: [u64];
+    gateway_invalidate_session: [bool];
+    gateway_reconnect: [];
+    gift_code_update: [];
+    guild_create: [Box<GuildCreate>];
+    guild_delete: [Box<GuildDelete>];
+    guild_emojis_update: [GuildEmojisUpdate];
+    guild_integrations_update: [GuildIntegrationsUpdate];
+    guild_update: [Box<GuildUpdate>];
+    invite_create: [Box<InviteCreate>];
+    invite_delete: [InviteDelete];
+    
 }
 
 // #[async_trait]
