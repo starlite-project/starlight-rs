12c0a1dd9168d16760925bd0900c9e76a01fa23f
Gryffon Bellish <owenbellish@gmail.com>
2021-07-23 18:49:49 +0000

    testing command structures

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -1,3 +1,4 @@
+pub mod commands;
 pub mod state;
 
 pub type GenericResult<T> = Result<T, Box<dyn std::error::Error + Send + Sync>>;
--- test/src/main.rs
+++ test/src/main.rs
@@ -1,18 +1,67 @@
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
+use twilight_model::application::command::{
+    BaseCommandOptionData, ChoiceCommandOptionData, Command, CommandOption,
+};
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(untagged)]
-enum Data {
-    Integer(u64),
-    Pair(String, String)
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct StarCommand {
+    pub name: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub default_permissions: Option<bool>,
+    pub description: String,
+    #[serde(default)]
+    pub options: Vec<CommandOption>,
 }
 
-fn main() {
-    let data = Data::Integer(10);
+impl PartialEq<Command> for StarCommand {
+    fn eq(&self, other: &Command) -> bool {
+        (
+            &self.name,
+            self.default_permissions,
+            &self.description,
+            &self.options,
+        ) == (
+            &other.name,
+            other.default_permission,
+            &other.description,
+            &other.options,
+        )
+    }
+}
 
-    println!("{}", serde_json::to_string_pretty(&data).unwrap());
+impl From<StarCommand> for Command {
+    fn from(value: StarCommand) -> Command {
+        Self {
+            application_id: None,
+            guild_id: None,
+            name: value.name,
+            default_permission: value.default_permissions,
+            description: value.description,
+            id: None,
+            options: value.options,
+        }
+    }
+}
 
-    let data = "10";
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let val = StarCommand {
+        name: "ping".to_owned(),
+        default_permissions: None,
+        description: "Pings the bot".to_owned(),
+        options: vec![],
+    };
 
-    dbg!(serde_json::from_str::<Data>(&data).unwrap());
+    let json = serde_json::to_string_pretty(&val)?;
+
+    eprintln!("{}", &json);
+
+    let val: StarCommand = serde_json::from_str(&json)?;
+
+    let cmd: Command = val.clone().into();
+
+    dbg!(val);
+
+    dbg!(cmd);
+
+    Ok(())
 }
