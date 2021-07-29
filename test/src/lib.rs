7f9932d56ec0aba5c40e0b686e809321041fd704
Gryffon Bellish <owenbellish@gmail.com>
2021-07-29 13:08:56 +0000

    more tests

--- test/src/lib.rs
+++ test/src/lib.rs
@@ -1,8 +1,27 @@
 #[derive(Debug, Default, Clone, Copy)]
 pub struct Coordinate(f64);
 
+impl<T> From<T> for Coordinate
+where
+    T: Into<f64>,
+{
+    fn from(value: T) -> Self {
+        Self(value.into())
+    }
+}
+
 #[derive(Debug, Default, Clone, Copy)]
 pub struct Point(Coordinate, Coordinate);
+
+impl<T, U> From<(T, U)> for Point
+where
+    T: Into<f64>,
+    U: Into<f64>,
+{
+    fn from((x, y): (T, U)) -> Self {
+        Self(Coordinate::from(x), Coordinate::from(y))
+    }
+}
 
 #[derive(Debug, Default, Clone)]
 pub struct Grid(Vec<Point>);
--- test/src/main.rs
+++ test/src/main.rs
@@ -1,5 +1,29 @@
-fn main() {
-    let val = 10;
+use self_meter::Meter;
+use star_test::Grid;
+use std::{
+    collections::BTreeMap,
+    io::{stderr, Write},
+    time::Duration,
+    thread::sleep
+};
 
-    dbg!(val);
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut meter = Meter::new(Duration::new(1, 0))?;
+    meter.track_current_thread("main");
+    loop {
+        meter
+            .scan()
+            .map_err(|e| writeln!(&mut stderr(), "Scan error: {}", e))
+            .ok();
+
+        println!("Report: {:?}", meter.report());
+        println!(
+            "Threads: {:?}",
+            meter.thread_report().map(|x| x.collect::<BTreeMap<_, _>>())
+        );
+
+        let val = Box::new(10);
+
+        sleep(Duration::new(1, 0));
+    }
 }
