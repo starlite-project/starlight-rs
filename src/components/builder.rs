c6c1c6d43cc41732680b71e4eed51d98bc6dd3a5
Gryffon Bellish <owenbellish@gmail.com>
2021-08-12 13:27:00 +0000

    trait tests

--- src/components/builder.rs
+++ src/components/builder.rs
@@ -22,3 +22,61 @@ impl Display for BuildError {
 }
 
 impl Error for BuildError {}
+
+#[cfg(test)]
+mod tests {
+    use super::{BuildError, ComponentBuilder};
+    use static_assertions::{assert_impl_all, assert_obj_safe};
+    use std::{error::Error, fmt::Debug};
+    use twilight_model::application::component::Component;
+
+    assert_obj_safe!(ComponentBuilder<Target = ()>);
+
+    assert_impl_all!(BuildError: Debug, Error, Send, Sync);
+
+    #[test]
+    #[should_panic]
+    fn builder_fail() {
+        #[derive(Clone, Copy)]
+        struct Fail;
+
+        impl ComponentBuilder for Fail {
+            type Target = ();
+
+            fn build(self) -> Result<Self::Target, BuildError> {
+                Err(BuildError)
+            }
+
+            fn build_component(self) -> Result<Component, BuildError> {
+                Err(BuildError)
+            }
+        }
+
+        let value = Fail;
+
+        value.build().unwrap();
+    }
+
+    #[test]
+    fn builder_pass() -> Result<(), BuildError> {
+        struct Builder;
+
+        impl ComponentBuilder for Builder {
+            type Target = u64;
+
+            fn build(self) -> Result<Self::Target, BuildError> {
+                Ok(10)
+            }
+
+            fn build_component(self) -> Result<Component, BuildError> {
+                Err(BuildError)
+            }
+        }
+
+        let builder = Builder;
+
+        assert_eq!(builder.build()?, 10);
+
+        Ok(())
+    }
+}
