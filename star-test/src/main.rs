2fb34dc4243184eb1b8a551df2f66eacb12fd1e3
Gryffon Bellish <owenbellish@gmail.com>
2021-10-09 11:52:37 -0400

    use clokwerk instead, need to make a custom wrapper

--- star-test/src/main.rs
+++ star-test/src/main.rs
@@ -1,14 +1,1 @@
-use click_derive::*;
-use twilight_model::application::component::button::ButtonStyle;
-
-trait ClickCommand<const N: usize> {
-	const LABELS: [&'static str; N];
-	const STYLES: [ButtonStyle; N];
-	const LINKS: &'static [(usize, &'static str)] = &[];
-}
-
-#[derive(ClickCommand)]
-#[buttons(Danger("Hello, world!"), Link("Testing", "https://github.com"))]
-struct TestCommand;
-
 fn main() {}
