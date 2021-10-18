d2dd888269434a0767dc0094e915c5e8c90661ef
Gryffon Bellish <owenbellish@gmail.com>
2021-10-18 12:45:13 +0000

    fix container maybe

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,28 @@
+apt-get update
+apt-get install -y \
+  curl \
+  git \
+  gnupg2 \
+  jq \
+  sudo \
+  zsh \
+  vim \
+  build-essential \
+  openssl \
+  cmake
+
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+rustup install nightly
+rustup component add rustfmt
+rustup component add rustfmt --toolchain nightly
+rustup component add clippy 
+rustup component add clippy --toolchain nightly
+
+cargo install cargo-expand
+cargo install cargo-edit
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+cp -R /root/.oh-my-zsh /home/$USERNAME
+cp /root/.zshrc /home/$USERNAME
+sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
+chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
