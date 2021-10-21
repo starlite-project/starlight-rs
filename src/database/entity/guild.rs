fb459b28c00df6d48d34296ead41a6603c251b1f
Gryffon <owenbellish@gmail.com>
2021-10-21 18:53:55 +0000

    add stuff

--- src/database/entity/guild.rs
+++ src/database/entity/guild.rs
@@ -1,13 +1,36 @@
-use sea_orm::entity::prelude::*;
-use serde::{Serialize ,Deserialize};
+use super::EntityDefinition;
 use crate::make_model;
+use async_trait::async_trait;
+use sea_orm::{
+	entity::prelude::*,
+	sea_query::{ColumnDef, Table},
+	ExecResult, StatementBuilder,
+};
+use serde::{Deserialize, Serialize};
 
 make_model!(GuildSettings);
 
 #[derive(Debug, Clone, Copy, PartialEq, Serialize, Deserialize, DeriveEntityModel)]
 #[sea_orm(table_name = "guilds")]
 pub struct Model {
-    #[sea_orm(primary_key)]
-    #[serde(skip_deserializing)]
-    pub id: u64
+	#[sea_orm(primary_key)]
+	#[serde(skip_deserializing)]
+	pub id: u64,
+}
+
+#[async_trait]
+impl EntityDefinition for GuildSettings {
+	async fn create_table(conn: &DbConn) -> Result<ExecResult, DbErr> {
+		let table = Table::create()
+		.table(Entity)
+		.to_owned();
+
+		Self::execute(conn, &table).await
+	}
+
+	async fn drop_table(conn: &DbConn) -> Result<ExecResult, DbErr> {
+		let table = Table::drop().table(Entity).clone();
+
+        Self::execute(conn, &table).await
+	}
 }
--- src/database/entity/mod.rs
+++ src/database/entity/mod.rs
@@ -1,21 +1,36 @@
 pub mod guild;
 
+use async_trait::async_trait;
+use sea_orm::{ConnectionTrait, DbConn, DbErr, ExecResult, Iden, StatementBuilder, sea_query::Table};
+
 #[doc(inline)]
 pub use self::guild::GuildSettings;
+
+#[async_trait]
+pub trait EntityDefinition: Iden + Sized {
+	async fn create_table(conn: &DbConn) -> Result<ExecResult, DbErr>;
+
+	async fn drop_table(conn: &DbConn) -> Result<ExecResult, DbErr>;
+
+    async fn execute<T: StatementBuilder + Sync>(conn: &DbConn, stmt: &T) -> Result<ExecResult, DbErr> {
+        let builder = conn.get_database_backend();
+        conn.execute(builder.build(stmt)).await
+    }
+}
 
 #[doc(hidden)]
 #[macro_export]
 macro_rules! make_model {
-    () => {
-        #[derive(Debug, Clone, Copy, PartialEq, sea_orm::EnumIter, sea_orm::DeriveRelation)]
-        pub enum Relation {}
+	() => {
+		#[derive(Debug, Clone, Copy, PartialEq, sea_orm::EnumIter, sea_orm::DeriveRelation)]
+		pub enum Relation {}
 
-        impl sea_orm::ActiveModelBehavior for ActiveModel {}
-    };
-    ($name:ident) => {
-        #[allow(dead_code)]
-        pub type $name = self::Entity;
+		impl sea_orm::ActiveModelBehavior for ActiveModel {}
+	};
+	($name:ident) => {
+		#[allow(dead_code)]
+		pub type $name = self::Entity;
 
-        make_model!();
-    }
+		make_model!();
+	};
 }
