4421fc98850dcf6b58ce7ff355551d84fbef6913
Gryffon <owenbellish@gmail.com>
2021-12-20 11:29:33 +0000

    just adapt my current impl

--- src/slashies/impl.rs
+++ src/slashies/impl.rs
@@ -1,19 +1,10 @@
 use std::pin::Pin;
 
 use futures_util::Future;
-use twilight_model::{
-	application::{
-		callback::{Autocomplete, CallbackData},
-		command::CommandOptionChoice,
-		interaction::{application_command::CommandData, ApplicationCommand},
-	},
-	channel::{
-		embed::Embed,
-		message::{allowed_mentions::AllowedMentionsBuilder, MessageFlags},
-	},
-};
-use twilight_util::builder::command::{CommandBuilder, SubCommandGroupBuilder};
+use twilight_model::application::interaction::application_command::CommandData;
+use twilight_util::builder::command::{CommandBuilder, SubCommandBuilder, SubCommandGroupBuilder};
 
+use super::SlashData;
 use crate::{helpers::InteractionsHelper, prelude::*};
 
 pub trait SlashCommand: Send + Sync {
@@ -37,98 +28,4 @@ pub trait DefineCommand: SlashCommand + Sized {
 	fn define() -> CommandBuilder;
 
 	fn parse(data: CommandData) -> MietteResult<Self>;
-}
-
-pub trait SubCommandCommand {
-	fn define_groups() -> SubCommandGroupBuilder;
-}
-
-#[derive(Debug, Clone)]
-#[must_use = "SlashData has no side effects"]
-pub struct SlashData {
-	pub callback: CallbackData,
-	pub command: ApplicationCommand,
-	pub autocomplete: Autocomplete,
-}
-
-impl SlashData {
-	pub const BASE: CallbackData = CallbackData {
-		allowed_mentions: None,
-		content: None,
-		embeds: vec![],
-		flags: None,
-		components: None,
-		tts: None,
-	};
-
-	pub const fn new(command: ApplicationCommand) -> Self {
-		Self {
-			callback: Self::BASE,
-			command,
-			autocomplete: Autocomplete { choices: vec![] },
-		}
-	}
-
-	pub fn allowed_mentions<F: FnOnce(AllowedMentionsBuilder) -> AllowedMentionsBuilder>(
-		&mut self,
-		builder: F,
-	) -> &mut Self {
-		self.callback.allowed_mentions = Some(builder(AllowedMentionsBuilder::new()).build());
-
-		self
-	}
-
-	pub fn message<T: AsRef<str>>(&mut self, content: T) -> &mut Self {
-		assert!(!content.as_ref().is_empty(), "empty message not allowed");
-
-		self.callback.content = Some(content.as_ref().to_owned());
-
-		self
-	}
-
-	pub fn autocomplete(&mut self, choices: Vec<CommandOptionChoice>) -> &mut Self {
-		self.autocomplete = Autocomplete { choices };
-
-		self
-	}
-
-	pub fn embeds(&mut self, embeds: Vec<Embed>) -> &mut Self {
-		assert!(!embeds.is_empty(), "empty embeds not allowed");
-
-		self.callback.embeds.extend(embeds);
-
-		self
-	}
-
-	pub fn embed(&mut self, embed: Embed) -> &mut Self {
-		self.embeds(vec![embed])
-	}
-
-	pub fn flags(&mut self, flags: MessageFlags) -> &mut Self {
-		self.callback.flags = self
-			.callback
-			.flags
-			.map_or(Some(flags), |current_flags| Some(flags | current_flags));
-
-		self
-	}
-
-	pub fn ephemeral(&mut self) -> &mut Self {
-		self.flags(MessageFlags::EPHEMERAL)
-	}
-
-	pub fn take(&mut self) -> Self {
-		Self {
-			callback: CallbackData {
-				allowed_mentions: self.callback.allowed_mentions.take(),
-				components: self.callback.components.take(),
-				content: self.callback.content.take(),
-				embeds: self.callback.embeds.clone(),
-				flags: self.callback.flags.take(),
-				tts: self.callback.tts.take(),
-			},
-			command: self.command.clone(),
-			autocomplete: self.autocomplete.clone(),
-		}
-	}
 }
--- src/slashies/mod.rs
+++ src/slashies/mod.rs
@@ -1,4 +1,96 @@
 pub mod commands;
 mod r#impl;
 
-pub use self::r#impl::{DefineCommand, SlashCommand, SlashData};
+use twilight_model::{application::{callback::{Autocomplete, CallbackData}, command::CommandOptionChoice, interaction::ApplicationCommand}, channel::{embed::Embed, message::{MessageFlags, allowed_mentions::AllowedMentionsBuilder}}};
+
+pub use self::r#impl::{DefineCommand, SlashCommand};
+
+#[derive(Debug, Clone)]
+#[must_use = "SlashData has no side effects"]
+pub struct SlashData {
+	pub callback: CallbackData,
+	pub command: ApplicationCommand,
+	pub autocomplete: Autocomplete,
+}
+
+impl SlashData {
+	pub const BASE: CallbackData = CallbackData {
+		allowed_mentions: None,
+		content: None,
+		embeds: vec![],
+		flags: None,
+		components: None,
+		tts: None,
+	};
+
+	pub const fn new(command: ApplicationCommand) -> Self {
+		Self {
+			callback: Self::BASE,
+			command,
+			autocomplete: Autocomplete { choices: vec![] },
+		}
+	}
+
+	pub fn allowed_mentions<F: FnOnce(AllowedMentionsBuilder) -> AllowedMentionsBuilder>(
+		&mut self,
+		builder: F,
+	) -> &mut Self {
+		self.callback.allowed_mentions = Some(builder(AllowedMentionsBuilder::new()).build());
+
+		self
+	}
+
+	pub fn message<T: AsRef<str>>(&mut self, content: T) -> &mut Self {
+		assert!(!content.as_ref().is_empty(), "empty message not allowed");
+
+		self.callback.content = Some(content.as_ref().to_owned());
+
+		self
+	}
+
+	pub fn autocomplete(&mut self, choices: Vec<CommandOptionChoice>) -> &mut Self {
+		self.autocomplete = Autocomplete { choices };
+
+		self
+	}
+
+	pub fn embeds(&mut self, embeds: Vec<Embed>) -> &mut Self {
+		assert!(!embeds.is_empty(), "empty embeds not allowed");
+
+		self.callback.embeds.extend(embeds);
+
+		self
+	}
+
+	pub fn embed(&mut self, embed: Embed) -> &mut Self {
+		self.embeds(vec![embed])
+	}
+
+	pub fn flags(&mut self, flags: MessageFlags) -> &mut Self {
+		self.callback.flags = self
+			.callback
+			.flags
+			.map_or(Some(flags), |current_flags| Some(flags | current_flags));
+
+		self
+	}
+
+	pub fn ephemeral(&mut self) -> &mut Self {
+		self.flags(MessageFlags::EPHEMERAL)
+	}
+
+	pub fn take(&mut self) -> Self {
+		Self {
+			callback: CallbackData {
+				allowed_mentions: self.callback.allowed_mentions.take(),
+				components: self.callback.components.take(),
+				content: self.callback.content.take(),
+				embeds: self.callback.embeds.clone(),
+				flags: self.callback.flags.take(),
+				tts: self.callback.tts.take(),
+			},
+			command: self.command.clone(),
+			autocomplete: self.autocomplete.clone(),
+		}
+	}
+}
